3a2b0c4aa4d3abc64a670d6ceb17740aad560042
sebasvisser <sebas.visser@mensenrelatie.nl>
2023-05-30 13:23:23 +0200

    opdracht afgemaakt volgens mij

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -9,36 +11,32 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Translator translator = new Translator(alphabetic, numeric);
 
-        while(play){
+        while(play) {
             System.out.println("Type 'x' om te stoppen \n Type 'v' om te vertalen");
             String input = scanner.nextLine();
+
             if(input.equals("x") ){
                 play = false;
                 System.out.println("Bedankt en tot ziens.");
-                break;
-            }else if(){
-
+            } else if(input.equals("v")) {
+                System.out.println("Type een cijfer in van 0 t/m 9");
+                int numberInput = scanner.nextInt();
+                scanner.nextLine();
+                if (numberInput < 10){
+                    String result = translator.translate(numberInput);
+                    System.out.println("De vertaling van " + numberInput + " is " + result);
+                } else {
+                    System.out.println("ongeldig");
+                }
+            } else {
+                System.out.println("ongeldig");
             }
         }
-
-
-//        Maak in de body van de while-loop een if/else if/else statement.
-//        1. __if__: Als de `input` "x" is, dan zet je `play` naar false.
-//        2. __else if__: Als de `input` "v" is, dan print je eerst "Type een cijfer in van 0 t/m 9",
-//                vervolgens sla je het resultaat van `scanner.nextInt()` op in een variabele `int number`,
-//        voeg nog een `scanner.nextLine();` toe op de volgende regel (scanner.nextInt doet dit namelijk niet
-//                van zichzelf)
-//        en als laatste check je met een if-statement of
-//        1. __if__: `number` kleiner is dan 10, dan sla je de waarde van `translator.translate(number)` op in `String result` en print je
-//           `"De vertaling van " + number + " is " + result`.
-//        2. __else__: anders dan print je `ongeldig`
-//        4. __else__: Als de `input` dus iets anders dan "x" of "v" is, dan print je `ongeldig`
-
     }
 }
 
+
 class Translator {
-// Maak in de Translator class een `HashMap<Integer,String>` variabele met de naam `numericAlpha`.
     private HashMap<String, Integer> numericAlpha = new HashMap<>();
     public Translator(String[] alphabetic, Integer[] numeric) {
         numericAlpha = new HashMap<>();
